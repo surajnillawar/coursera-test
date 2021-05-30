cbbc251fae4f472549a684f9a80f056fd40d5f9e
surajnillawar <47081259+surajnillawar@users.noreply.github.com>
2021-05-30 17:19:00 +0530

    Update app.js

--- AngularJSCourse/assignment1/app.js
+++ AngularJSCourse/assignment1/app.js
@@ -13,8 +13,6 @@
       //  $scope.foodItems;
       $scope.foodItems = $scope.foodItems.trim();
 
-      console.log("inside fucntion", $scope.foodItems);
-
       if ($scope.foodItems == "") {
         $scope.message = "Please enter data in above text box";
       } else {
@@ -24,10 +22,8 @@
         var $length = $foodItemsArray.length;
 
         if ($length <= 3) {
-          console.log("inside if", $scope.foodItems);
           $scope.message = "Enjoy!";
         } else {
-          console.log("inside else", $scope.foodItems);
           $scope.message = "Too much!";
         }
       }
